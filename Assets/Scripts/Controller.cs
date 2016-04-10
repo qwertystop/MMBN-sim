521e03d0cfed4819bbceecf363f12feda13d5dd6
DHeyman <d.heyman256@gmail.com>
2016-04-10 16:23:27 -0400

    Quality settings simplified to allow locked framerate

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,6 +9,9 @@ public class Controller : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         gameCore = this;
+        // set framerate to 60fps cap
+        // technically the GBA ran at 59.8621 Hz, but A: can you really tell? and B: Unity only takes integer framerates
+        Application.targetFrameRate = 60;
         for (int i = 0; i < panels.Length; i++)
         {
             panels[i].GetComponent<Panel>().index = i;
