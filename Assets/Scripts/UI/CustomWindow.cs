f6af46c548f283e942cad8557a49861d9b29986b
DHeyman <d.heyman256@gmail.com>
2016-04-27 12:18:15 -0400

    Cursor movement complete

--- Assets/Scripts/UI/CustomWindow.cs
+++ Assets/Scripts/UI/CustomWindow.cs
@@ -29,9 +29,12 @@ public class CustomWindow : MonoBehaviour {
     private Image activeCode;
     private Image activeElement;
     private Image[] activeDamage = new Image[3];
-    // buttons
+    // buttons and cursor
     private Image add;
     private Image ok;
+    private Image cursor;
+    private readonly Vector2 cursorSizeOnButton = new Vector2(26, 17);
+    private readonly Vector2 cursorSizeOnChip = new Vector2(18, 18);
 
     // reference to Player
     private Player player;
@@ -71,9 +74,10 @@ public class CustomWindow : MonoBehaviour {
             activeDamage[i] = activeGroup.FindChild(i.ToString()).GetComponent<Image>();
         }
 
-        // buttons
+        // buttons and cursor
         ok = gameObject.FindChild("OK").GetComponent<Image>();
         add = gameObject.FindChild("Add").GetComponent<Image>();
+        cursor = gameObject.FindChild("Cursor").GetComponent<Image>();
 
         // and draw the first hand
         hand = Draw(handSize);
@@ -88,33 +92,46 @@ public class CustomWindow : MonoBehaviour {
         moveCursor();
 	}
 
+    // Runs after Update once per frame - ensures that changes here don't get overwritten by UI system
+    void LateUpdate() {
+        updateCursorRenderer();
+    }
+
     // moves cursor as appropriate for button input and current position
     // (includes wraparound)
-    void moveCursor() {
+    private void moveCursor() {
         int oldLoc = cursorLoc;
-        if (InputHandler.buttonHeld(player.playerNo, InputHandler.button.UP)) {
+        if (InputHandler.buttonDown(player.playerNo, InputHandler.button.UP)) {
             if (10 == cursorLoc) { cursorLoc = 11; }// on ADD, move to OK
             else if (11 == cursorLoc || cursorLoc < 5) { cursorLoc = 10; }// on OK or top row, move to ADD
             else { cursorLoc -= 5; }// on bottom row, move to top row
-        } else if (InputHandler.buttonHeld(player.playerNo, InputHandler.button.DOWN)) {
-            if (10 == cursorLoc) { cursorLoc = 4; }// on ADD, move to last of top row
+        } else if (InputHandler.buttonDown(player.playerNo, InputHandler.button.DOWN)) {
+            if (10 == cursorLoc) { cursorLoc = (player.playerNo ^ 1) * 4; }// on ADD, move to near end of top row (0 or 4)
             else if (11 == cursorLoc) { cursorLoc = 10; }// on OK, move to ADD
             else if (5 > cursorLoc) { cursorLoc += 5; }// on top row, move to bottom row
             else { cursorLoc -= 5; }// on bottom row, move to top row
-        } else if(InputHandler.buttonHeld(player.playerNo, InputHandler.button.LEFT)) {
-            if (10 == cursorLoc || 11 == cursorLoc) { cursorLoc = player.playerNo * 4; }// depending on player, move to near end of top row (0 or 4)
+        } else if(InputHandler.buttonDown(player.playerNo, InputHandler.button.LEFT)) {
+            if (10 == cursorLoc || 11 == cursorLoc) { cursorLoc = 4; }// on OK or ADD, move to right end of top row
             else if (0 == cursorLoc % 5) { cursorLoc = (0 == player.playerNo) ? 4 + cursorLoc : 10; }// p0 moves to opp. end of row, p1 moves to ADD
             else { cursorLoc -= 1; }// somewhere in row other than left end, move left
-        } else if (InputHandler.buttonHeld(player.playerNo, InputHandler.button.RIGHT)) {
-            if (10 == cursorLoc || 11 == cursorLoc) { cursorLoc = (player.playerNo ^ 1) * 4; }// depending on player, move to near end of top row (4 or 0)
+        } else if (InputHandler.buttonDown(player.playerNo, InputHandler.button.RIGHT)) {
+            if (10 == cursorLoc || 11 == cursorLoc) { cursorLoc = 0; }// on OK or ADD, move to left end of top row
             else if (4 == cursorLoc % 5) { cursorLoc = (0 == player.playerNo) ? 10 : cursorLoc - 4; }// p0 moves to ADD, p1 moves to opp. end of row
             else { cursorLoc += 1; }// somewhere in row other than right end, move right
         }
-        // update drawn cursor loc if it changed
-        if (oldLoc != cursorLoc)
-        {
-            //TODO
-            todotodotodo
+    }
+
+    private void updateCursorRenderer()
+    {// update drawn cursor locaction if it changed
+        //TODO
+        if (cursorLoc > 9)
+        {// on either Add or OK, resize to button size and move to that button
+            cursor.rectTransform.sizeDelta = cursorSizeOnButton;
+            cursor.rectTransform.position = (cursorLoc == 10) ? add.rectTransform.position : ok.rectTransform.position;
+        } else
+        {// on a chip, resize to chip size and move to that chip
+            cursor.rectTransform.sizeDelta = cursorSizeOnChip;
+            cursor.rectTransform.position = handRenderers[cursorLoc].rectTransform.position;
         }
     }
 
